7f1aa0579f3c38ce7e99232042f0bc7a38b83798
ahmed.abdelgawad.farag <ahmed.abdelgawad.farag@gmail.com>
2025-11-21 21:54:21 +0200

    Download lazy.nvim by default.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,20 @@
+-- Install lazy vim first if not installed.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- The below line set the CWD to vim argument instead of root directory
 -- Ex vim /dev/project ----> CWD will /dev/project
 vim.cmd([[autocmd VimEnter * if argc() > 0 | execute 'cd' fnameescape(fnamemodify(argv()[0], ':p:h')) | endif]])
